b409015effee5ed560e3f7ac458e06fe076459e7
PrashantAlagawadi <prashant30a@gmail.com>
2019-03-17 14:06:46 +0530

    new tables are added

--- server/fapi.py
+++ server/fapi.py
@@ -1,1 +1,43 @@
+from flask import Flask,jsonify
+import pymysql
+app=Flask(__name__)
 
+def db():
+	conn = pymysql.connect(host='localhost',
+						port=8686,user='root',
+						password='',
+						db='TaskManager')
+	cur = conn.cursor()
+	return conn,cur
+
+@app.route('/tasks/')
+def tasks():
+	conn, cur = db()
+	tasks = []
+	cur.execute("Select * From Tasks")
+	for row in cur:
+	
+		tasks.append(row)
+	return jsonify(tasks)
+
+
+@app.route('/users/')
+def users():
+	conn, cur = db()
+	users = []
+	cur.execute("Select * From users")
+	for row in cur:
+		users.append(row)
+	return jsonify(users)
+
+
+@app.route('/todaystask/')
+def todaystask():
+	conn, cur = db()
+	todaystask = []
+	cur.execute("Select * From todaystask")
+	for row in cur:
+		todaystask.append(row)
+	return jsonify(todaystask)
+
+app.run(debug=True)
